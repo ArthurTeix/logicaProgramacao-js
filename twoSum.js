6f66222b1c1ccf8acec068693f916cf08410be74
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-05-02 12:48:32 -0300

    docs: add comments on Two Sum

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,13 @@
+// Devo criar um array e retornar os índices de que a soma dos números respectivos resultem no meu target
 const array = [2, 3, 5, 7]
-const target = 9
-let map = new Map()
+const target = 9 // alvo de resultado
+let map = new Map() // para guardar valores e chegar depois
 
 for (let i = 0; i < array.length; i++){
-    let complement = target - array[i]
-    if (map.has(complement)){
-        console.log(`[${map.get(complement)}, ${i}]`)
-    } else {
-        map.set(array[i], i)
-        console.log(map)
+    let complement = target - array[i] // sempre fará 9 - (nº respectivo do array)
+    if (map.has(complement)){ // busca se já apareceu o complemento
+        console.log(`[${map.get(complement)}, ${i}]`) // se já tiver registrado, retorno o índice do número já registrado e o índice atual
+    } else { 
+        map.set(array[i], i) // se um complemento ainda não existir no map, ele será adicionado
     }
 }
