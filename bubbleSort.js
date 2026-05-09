279d1c8d30e852d933814b461ebc9c0b4af43c41
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-05-09 12:54:29 -0300

    feat: refactory bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -4,9 +4,9 @@ const bubbleSort = (array) => {
 
     const arrayNoSorted = [...array] // salvando array desordenado
 
-    for (let i = 0; i < array.length; i++){ // fazendo bubble em todos os termos do array
+    for (let i = 0; i < array.length - 1; i++){ // fazendo bubble em todos os termos do array
 
-        for (let j = 0; j < array.length -1; j++) {
+        for (let j = 0; j < array.length - 1 - i; j++) { // -i para evitar comparações desnecessárias
 
             if (array[j] > array[j + 1]) { // se um item é maior que seu posterior, ele avança
                 [array[j], array[j+1]] = [array[j+1], array[j]] // desestruturação para organizar
@@ -17,4 +17,4 @@ const bubbleSort = (array) => {
     return `[${[arrayNoSorted]}] -> [${[...array]}]` // array antigo -> array ordenado
 }
 
-console.log(bubbleSort([5, 1, 3, 4]))
+console.log(bubbleSort([5, 1, 3, 4, 8, 2, 1]))
