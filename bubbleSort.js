3f9ef0704862f4144b8938f850fb2e7db0181650
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-05-09 04:09:14 -0300

    feat: add bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,20 @@
+// Organizar um array em ordem crescente
+
+const bubbleSort = (array) => {
+
+    const arrayNoSorted = [...array] // salvando array desordenado
+
+    for (let i = 0; i < array.length; i++){ // fazendo bubble em todos os termos do array
+
+        for (let j = 0; j < array.length -1; j++) {
+
+            if (array[j] > array[j + 1]) { // se um item é maior que seu posterior, ele avança
+                [array[j], array[j+1]] = [array[j+1], array[j]] // desestruturação para organizar
+            }
+    }
+    }
+
+    return `[${[arrayNoSorted]}] -> [${[...array]}]` // array antigo -> array ordenado
+}
+
+console.log(bubbleSort([5, 1, 3, 4]))
