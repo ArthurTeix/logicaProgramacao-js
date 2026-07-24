eb780de77758111822cc38c2f0d01715893b1433
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-07-23 23:31:05 -0300

    feat: add balanced parentheses

--- balancedParentheses.js
+++ balancedParentheses.js
@@ -1,20 +1,29 @@
-// desafio é validar apenas parenteses que foram abertos e fechados corretamente
+// desafio é validar apenas parenteses que foram abertos e fechados corretamente usando switch
 
 const isBalanced = (txt) => {
     let cont = 0
     let balanced = true
 
     for (let value of txt) {
+
+        if (cont < 0) balanced = false
+
         switch (value) {
             case '(':
                 cont += 1
+                break
             case ')':
                 cont -= 1
-
-        if (value < 0) balanced = false
-        if (cont != 0) balanced = false
+                break
         }
     }
 
-    console.log(`Está balanceado? ${balanced}`)
+    if (cont != 0) balanced = false
+
+    return balanced
 }
+
+console.log(isBalanced("()"))
+console.log(isBalanced(")("))
+console.log(isBalanced("())"))
+console.log(isBalanced("(()"))
